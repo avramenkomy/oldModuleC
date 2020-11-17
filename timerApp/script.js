2b4f9d84f9bb2246854a2aa5083ad1ba46e2a72a
avramenkomy <avramenkomy@yandex.ru>
2020-11-17 17:27:50 +0300

    oldModuleC-master: add index, style, script files in timerApp

---
+++ timerApp/script.js
@@ -0,0 +1,6 @@
+const btnSound = document.querySelector('.j-btn-sound');
+// const sound = document.querySelector('#sound1');
+
+btnSound.addEventListener('click', () => {
+    document.querySelector('#sound1').play();
+});
