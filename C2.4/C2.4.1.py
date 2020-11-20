b7d45f40087f96e0232d3b898e2761ec09697829
avramenkomy <avramenkomy@yandex.ru>
2020-11-20 13:44:39 +0300

    oldModuleC-master: add example work with bottle

---
+++ C2.4/C2.4.1.py
@@ -0,0 +1,5 @@
+import time
+words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
+for word in words:
+    print(word)
+    time.sleep(2)
---
+++ C2.4/C2.4.3.py
@@ -0,0 +1,12 @@
+import time
+import bottle
+
+@bottle.route('/words')
+def word_spammer():
+    words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
+    for word in words:
+        yield word
+        time.sleep(2)
+
+if __name__ == "__main__":
+    bottle.run(server='waitress')
