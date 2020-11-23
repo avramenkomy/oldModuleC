cba07d13a93880ab26dbd0a303501e9bd8978e8e
avramenkomy <avramenkomy@yandex.ru>
2020-11-23 14:06:33 +0300

    C2-C2_webinar: add files

--- C2.5/C2.5.1.py
+++ C2.5/C2.5.1.py
@@ -4,11 +4,12 @@ import bottle
 def word_spammer():
     bottle.response.content_type = "text/event-stream"
     bottle.response.cache_control = "no-cache"
-    bottle.response.headers['Access-Constrol-Allow-Origin'] = 'https://sf-pyw.mosyag.in/'
+    bootle.response.connection = "keep-alive"
+    bottle.response.headers['Access-Constrol-Allow-Origin'] = '*'
 
 
     hello = 'hello, world'
     return 'data: %s\n\n' % hello
 
 if __name__ == "__main__":
-    bottle.run(host='localhost', port=9999, debug=True)
+    bottle.run(host='waitress')
